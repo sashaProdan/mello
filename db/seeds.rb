fda9120bfed351b6a86724b2dafcd7cc4bef5dac
Sasha Prodan <prodan.sasha@gmail.com>
2018-10-24 11:13:49 -0500

    Seed database
    Co-authored: nick calibey <nick.calibey@gmail.com>
    Co-authored: sasha prodan <prodan.sasha@gmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Board.destroy_all
+
+board = Board.create({
+  title: "My Todos"
+})
+board.save
+
+list1 = board.lists.create({
+  title: "List 1"
+})
+list1.save
+
+list2 = board.lists.create({
+  title: "List 2"
+})
+list2.save
+
+list1.cards.create({ title: "Card 1" }).save
+list1.cards.create({ title: "Card 2" }).save
+
+list2.cards.create({ title: "Card 1" }).save
+list2.cards.create({ title: "Card 2" }).save
+list2.cards.create({ title: "Card 3" }).save
+
+
