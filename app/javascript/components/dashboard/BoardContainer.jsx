4a5877a37fa79cde6c5be228d79bb6fa7b7f8765
Nick Calibey <nick.calibey@gmail.com>
2018-10-25 10:53:09 -0500

    added routing path for individual boards

--- app/javascript/components/dashboard/BoardContainer.jsx
+++ app/javascript/components/dashboard/BoardContainer.jsx
@@ -28,29 +28,32 @@ class BoardContainer extends React.Component {
         boardId={list.board_id}
       />
   ));
-
-    return (
-      <div>
-        <header>
-          <ul>
-            <li id="title">{board.title}</li>
-            <li className="star-icon icon"></li>
-            <li className="private private-icon icon">Private</li>
-          </ul>
-          <div className="menu">
-            <i className="more-icon sm-icon"></i>Show Menu</div>
-          <div className="subscribed">
-            <i className="sub-icon sm-icon"></i>Subscribed</div>
-        </header>
-        <main>
-          <div id="list-container" className="list-container">
-            <div id="existing-lists" className="existing-lists">
-              {listComponents}
+    if (board) {
+      return (
+        <div>
+          <header>
+            <ul>
+              <li id="title">{board.title}</li>
+              <li className="star-icon icon"></li>
+              <li className="private private-icon icon">Private</li>
+            </ul>
+            <div className="menu">
+              <i className="more-icon sm-icon"></i>Show Menu</div>
+            <div className="subscribed">
+              <i className="sub-icon sm-icon"></i>Subscribed</div>
+          </header>
+          <main>
+            <div id="list-container" className="list-container">
+              <div id="existing-lists" className="existing-lists">
+                {listComponents}
+              </div>
             </div>
-          </div>
-        </main>
-      </div>
-    )
+          </main>
+        </div>
+      )
+    } else {
+      return (null)
+    }
   }
 }
 
