4f6af9223d75c8ffe8f0005e175d8e5cefaff0a0
Nick Calibey <nick.calibey@gmail.com>
2018-10-25 14:30:29 -0500

    added PUT /api/lists/:id endpoint
    Co-authored-by: nick calibey <nick.calibey@gmail.com>
    Co-authored-by: sasha prodan <prodan.sasha@gmail.com>

--- app/controllers/api/lists_controller.rb
+++ app/controllers/api/lists_controller.rb
@@ -15,6 +15,23 @@ class Api::ListsController < ApplicationController
     render 'api/shared/error', status: 404
   end
 
+  def update
+    @list = List.find(params[:id])
+
+    if @list.update(list_params)
+      render :update, status: 200
+    else
+      @error = @list.errors.full_messages.join(', ')
+      render 'api/shared/error', status: :unprocessable_entity
+    end
+  rescue ActionController::ParameterMissing
+    @error = "Invalid list data provided"
+    render 'api/shared/error', status: :unprocessable_entity
+  rescue ActiveRecord::RecordNotFound
+    @error = 'No list with that id exists'
+    render 'api/shared/error', status: 404
+  end
+
   private
 
   def list_params
