e4b8df8f886f66f4a62fb256d888d4ba03bbbe66
Nick Calibey <nick.calibey@gmail.com>
2018-10-25 11:47:34 -0500

    began converting CardTile to dynamic component
    Co-authored-by: nick calibey <nick.calibey@gmail.com>
    Co-authored-by: sasha prodan <prodan.sasha@gmail.com>

--- app/javascript/components/dashboard/CardTile.jsx
+++ app/javascript/components/dashboard/CardTile.jsx
@@ -2,17 +2,26 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class CardTile extends React.Component {
+  static contextTypes = {
+    store: PropTypes.object,
+  };
+
   render() {
-    return(
+    const card = this.context.store.getState().cards.find(card => card.id === this.props.id);
+    const labels = card.labels.map(label => (
+      <div className={"card-label" + label + "colorblindable"}></div>
+    ));
+
+    return (
       <div className="card-background">
-        <div className="card "><i className="edit-toggle edit-icon sm-icon"></i>
-          <div className="cover-image"></div>
-          <div className="card-info">
-              <p>Add members to a board (via the sidebar to collaborate, share and discuss.</p>
+          <div className="card "><i className="edit-toggle edit-icon sm-icon"></i>
+              <div className="card-info">
+                  {card.labels.length > 0 ? labels : ''}
+                  <p>{card.description}</p>
+              </div>
+              <div className="card-icons"><i className="clock-icon sm-icon overdue-recent completed">Aug 4</i><i className="description-icon sm-icon"></i><i className="comment-icon sm-icon"></i>
+              </div>
           </div>
-          <div className="card-icons"><i className="clock-icon sm-icon due-soon ">Sep 5</i>
-          </div>
-        </div>
       </div>
     )
   }
