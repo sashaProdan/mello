50ba6159287883b9c53988dedddd067d0d15af25
Nick Calibey <nick.calibey@gmail.com>
2018-10-25 11:29:28 -0500

    added static CardTile Component
    Co-authored-by: nick calibey <nick.calibey@gmail.com>
    Co-authored-by: sasha prodan <prodan.sasha@gmail.com>

---
+++ app/javascript/components/dashboard/CardTile.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class CardTile extends React.Component {
+  render() {
+    return(
+      <div className="card-background">
+        <div className="card "><i className="edit-toggle edit-icon sm-icon"></i>
+          <div className="cover-image"></div>
+          <div className="card-info">
+              <p>Add members to a board (via the sidebar to collaborate, share and discuss.</p>
+          </div>
+          <div className="card-icons"><i className="clock-icon sm-icon due-soon ">Sep 5</i>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+
+export default CardTile;
--- app/javascript/components/dashboard/CardsContainer.jsx
+++ app/javascript/components/dashboard/CardsContainer.jsx
@@ -1,31 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import CardTile from './CardTile';
 
 import * as actions from '../../actions/BoardActions';
 
 class CardsContainer extends React.Component {
+  static contextTypes = {
+    store: PropTypes.object,
+  };
+
   render() {
+    const id = this.props.listId;
+    const cards = this.context.store.getState().cards.filter(card => card.list_id === id);
+    const cardComponents = cards.map(card => (
+      <CardTile
+        key={card.id}
+        id={card.id}
+      />
+    ));
+
     return(
       <div id="cards-container" data-id="list-2-cards">
-        <div className="card-background">
-            <div className="card "><i className="edit-toggle edit-icon sm-icon"></i>
-                <div className="cover-image"></div>
-                <div className="card-info">
-                    <p>Add members to a board (via the sidebar to collaborate, share and discuss.</p>
-                </div>
-                <div className="card-icons"><i className="clock-icon sm-icon due-soon ">Sep 5</i>
-                </div>
-            </div>
-        </div>
-        <div className="card-background">
-            <div className="card "><i className="edit-toggle edit-icon sm-icon"></i>
-                <div className="cover-image"></div>
-                <div className="card-info">
-                    <p>You can also change the background and more.</p>
-                </div>
-                <div className="card-icons"></div>
-            </div>
-        </div>
+        {cardComponents}
       </div>
     )
   }
