849c20b2d896060511592c7d89521db9e847aafc
Nick Calibey <nick.calibey@gmail.com>
2018-10-25 11:38:13 -0500

    added static AddCardForm Component
    Co-authored-by: nick calibey <nick.calibey@gmail.com>
    Co-authored-by: sasha prodan <prodan.sasha@gmail.com>

---
+++ app/javascript/components/dashboard/AddCardForm.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class AddCardForm extends React.Component {
+  render() {
+    return (
+      <div className="add-card-toggle" data-position="bottom">Add a card...</div>
+    )
+  }
+}
+
+export default AddCardForm;
--- app/javascript/components/dashboard/List.jsx
+++ app/javascript/components/dashboard/List.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import CardsContainer from './CardsContainer';
+import AddCardForm from './AddCardForm';
 
 import * as actions from '../../actions/BoardActions';
 
@@ -21,26 +22,26 @@ class List extends React.Component {
       <div className="list-wrapper">
         <div className="list-background">
           <div className="list">
-              <a className="more-icon sm-icon" href=""></a>
-              <div>
-                  <input type="text" className="list-title" value={list.title} autofocus="true" />
-              </div>
-              <div className="add-dropdown add-top">
-                  <div className="card"></div><a className="button">Add</a><i className="x-icon icon"></i>
-                  <div className="add-options"><span>...</span>
-                  </div>
-              </div>
-              <CardsContainer
-                listId={id}
-              />
-                <div className="add-dropdown add-bottom">
-                    <div className="card"><div className="card-info"></div><textarea name="add-card"></textarea><div className="members"></div></div>
-                    <a className="button">Add</a><i className="x-icon icon"></i>
-                    <div className="add-options"><span>...</span>
-                    </div>
+            <a className="more-icon sm-icon" href=""></a>
+            <div>
+                <input type="text" className="list-title" value={list.title} autofocus="true" />
+            </div>
+            <div className="add-dropdown add-top">
+                <div className="card"></div><a className="button">Add</a><i className="x-icon icon"></i>
+                <div className="add-options"><span>...</span>
                 </div>
-                <div className="add-card-toggle" data-position="bottom">Add a card...</div>
-              </div>
+            </div>
+            <CardsContainer
+              listId={id}
+            />
+            <div className="add-dropdown add-bottom">
+                <div className="card"><div className="card-info"></div><textarea name="add-card"></textarea><div className="members"></div></div>
+                <a className="button">Add</a><i className="x-icon icon"></i>
+                <div className="add-options"><span>...</span>
+                </div>
+            </div>
+            <AddCardForm />
+          </div>
         </div>
       </div>
     )
