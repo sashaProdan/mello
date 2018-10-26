2e42630ec0d1ad6c48a43e3cce657a03cb5c7ec0
Nick Calibey <nick.calibey@gmail.com>
2018-10-26 13:36:42 -0500

    converted to use switch statement

--- app/javascript/reducers/BoardsReducer.js
+++ app/javascript/reducers/BoardsReducer.js
@@ -1,17 +1,18 @@
 export default function boardsReducer(state = [], action) {
-  if (action.type === 'FETCH_BOARDS_SUCCESS') {
-    return action.boards;
-  } else if (action.type === 'CREATE_BOARD_SUCCESS') {
-    const newBoard = action.board;
-    newBoard.id = Number(newBoard.id);
+  switch (action.type) {
+    case 'FETCH_BOARDS_SUCCESS':
+      return action.boards;
+    case 'CREATE_BOARD_SUCCESS':
+      const newBoard = action.board;
+      newBoard.id = Number(newBoard.id);
 
-    return state.concat(newBoard);
-  } else if (action.type === 'FETCH_BOARD_SUCCESS') {
-    const excludedBoards = state.filter(board => board.id !== action.board.id);
-    const { lists, ...newBoardWithoutLists } = action.board;
+      return state.concat(newBoard);
+    case 'FETCH_BOARD_SUCCESS':
+      const excludedBoards = state.filter(board => board.id !== action.board.id);
+      const { lists, ...newBoardWithoutLists } = action.board;
 
-    return excludedBoards.concat(newBoardWithoutLists);
-  } else {
-    return state;
+      return excludedBoards.concat(newBoardWithoutLists);
+    default:
+      return state;
   }
 }
