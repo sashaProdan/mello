16ae82bd325edb920a9a54e0357ce0f74e8872a6
Nick Calibey <nick.calibey@gmail.com>
2018-10-29 11:51:04 -0500

    added UI for adding a card
    Co-authored-by: nick calibey <nick.calibey@gmail.com>
    Co-authored-by: sasha prodan <prodan.sasha@gmail.com>

--- app/javascript/components/dashboard/AddCardForm.jsx
+++ app/javascript/components/dashboard/AddCardForm.jsx
@@ -2,10 +2,95 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class AddCardForm extends React.Component {
+  state = {
+    clicked: false,
+    fields: {
+      title: '',
+    }
+  }
+
+  componentDidUpdate() {
+    this.refs.textarea.focus();
+  }
+
+  resetState() {
+    this.setState({
+      clicked: false,
+      fields: {
+        title: '',
+      }
+    });
+  }
+
+  handleCancelClick = (e) => {
+    e.stopPropagation();
+
+    this.resetState();
+  }
+
+  handleClick = (e) => {
+    this.setState({ clicked: true });
+  }
+
+  handleBlur = (e) => {
+    if (e.target.id !== 'add') {
+      this.setState({ clicked: false });
+    }
+  }
+
+  handleChange = (e) => {
+    const fields = Object.assign({}, this.state.fields, {
+      title: e.target.value,
+    });
+
+    this.setState({ fields });
+  }
+
   render() {
-    return (
-      <div className="add-card-toggle" data-position="bottom">Add a card...</div>
-    )
+    const clicked = this.state.clicked;
+    const title = this.state.fields.title;
+
+    if (clicked) {
+      return (
+        <div className="add-dropdown-active active-card">
+          <div className="card">
+            <div className="card-info"></div>
+            <textarea
+              name="add-card"
+              placeholder="Enter a title for this card..."
+              ref='textarea'
+              onChange={this.handleChange}
+              onBlur={this.handleBlur}
+              value={title}
+            >
+            </textarea>
+            <div className="members"></div>
+          </div>
+          <a
+            id="add"
+            className="button">
+            Add
+          </a>
+          <i
+            className="x-icon icon"
+            onMouseDown={this.handleCancelClick}
+          >
+          </i>
+          <div className="add-options"><span>...</span>
+          </div>
+        </div>
+      )
+    } else {
+      return (
+        <div
+          className="add-card-toggle"
+          data-position="bottom"
+          onClick={this.handleClick}
+        >
+          Add a card...
+        </div>
+      )
+    }
   }
 }
 
