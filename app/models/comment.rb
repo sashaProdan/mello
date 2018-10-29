b3e94bc80b04f61065a18e597ae9ef57fd78979e
Nick Calibey <nick.calibey@gmail.com>
2018-10-29 10:35:38 -0500

    added comments model
    Co-authored-by: nick calibey <nick.calibey@gmail.com>
    Co-authored-by: sasha prodan <prodan.sasha@gmail.com>

---
+++ app/models/comment.rb
@@ -0,0 +1,5 @@
+class Comment < ApplicationRecord
+  belongs_to :card
+
+  validates_presence_of :text, allow_blank: false
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181023164639) do
+ActiveRecord::Schema.define(version: 20181029152301) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,14 @@ ActiveRecord::Schema.define(version: 20181023164639) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["list_id"], name: "index_cards_on_list_id"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.text "text"
+    t.bigint "card_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["card_id"], name: "index_comments_on_card_id"
   end
 
   create_table "lists", force: :cascade do |t|
