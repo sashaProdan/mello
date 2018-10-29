abc1922abe1d140f70313a92e33ed5a9b54d0d82
Sasha Prodan <prodan.sasha@gmail.com>
2018-10-29 12:16:08 -0500

    create migrations for adding position and completed card columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181029152301) do
+ActiveRecord::Schema.define(version: 20181029165948) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 20181029152301) do
     t.bigint "list_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "position", default: 0.0
+    t.boolean "completed", default: false
     t.index ["list_id"], name: "index_cards_on_list_id"
   end
 
