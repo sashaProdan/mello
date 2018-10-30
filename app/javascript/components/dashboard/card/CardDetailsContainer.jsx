26429b8528266666d5c03806ffeeb08ff8650ca5
Nick Calibey <nick.calibey@gmail.com>
2018-10-30 13:30:34 -0500

    added CardDescription component
    Co-authored-by: nick calibey <nick.calibey@gmail.com>
    Co-authored-by: sasha prodan <prodan.sasha@gmail.com>

--- app/javascript/components/dashboard/card/CardDetailsContainer.jsx
+++ app/javascript/components/dashboard/card/CardDetailsContainer.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 
 import CardLabels from './CardLabels';
 import CardDueDate from './CardDueDate';
+import CardDescription from './CardDescription';
 
 class CardDetailsContainer extends React.Component {
   render() {
@@ -18,13 +19,9 @@ class CardDetailsContainer extends React.Component {
             card={card}
           />
         </ul>
-        <form className="description">
-          <p>Description</p>
-          <span id="description-edit" className="link">Edit</span>
-          <p className="textarea-overlay">{card.description}</p>
-          <p id="description-edit-options" className="hidden">You have unsaved edits on this field. <span className="link">View edits</span> - <span className="link">Discard</span>
-          </p>
-        </form>
+          <CardDescription
+            card={card}
+          />
       </li>
     )
   }
