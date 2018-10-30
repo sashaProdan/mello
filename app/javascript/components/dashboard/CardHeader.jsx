4ba761ad40e782508456f44708f249461fae2cba
Nick Calibey <nick.calibey@gmail.com>
2018-10-30 12:29:40 -0500

    added dynamic CardHeader component
    Co-authored-by: nick calibey <nick.calibey@gmail.com>
    Co-authored-by: sasha prodan <prodan.sasha@gmail.com>

--- app/javascript/components/dashboard/CardHeader.jsx
+++ app/javascript/components/dashboard/CardHeader.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
 
 class CardHeader extends React.Component {
+  static contextTypes = {
+    store: PropTypes.object,
+  };
+
   render() {
+    const store = this.context.store
+    const card = store.getState().cards.find(card => card.id === this.props.id);
+    const list = store.getState().lists.find(list => list.id === card.list_id);
+
     return (
       <header>
         <i className="card-icon icon .close-modal"></i>
-        <textarea className="list-title" style={{height: '45px'}}>Cards do many cool things. Click on this card to open it and learn more...</textarea>
-        <p>in list <a className="link">Stuff to try (this is a list)</a><i className="sub-icon sm-icon"></i>
+        <textarea
+          className="list-title"
+          style={{height: '45px'}}
+          value={card.title}
+        >
+        </textarea>
+        <p>in list <a className="link">{list.title}</a><i className="sub-icon sm-icon"></i>
         </p>
       </header>
     )
