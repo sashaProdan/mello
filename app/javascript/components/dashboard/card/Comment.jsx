1e2a8c2b83503cf05e0544a8cfd77af120aa91be
Nick Calibey <nick.calibey@gmail.com>
2018-10-31 11:20:56 -0500

    comments GUI now dipslay JSON info
    Co-authored-by: nick calibey <nick.calibey@gmail.com>
    Co-authored-by: sasha prodan <prodan.sasha@gmail.com>

--- app/javascript/components/dashboard/card/Comment.jsx
+++ app/javascript/components/dashboard/card/Comment.jsx
@@ -1,20 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import moment from 'moment';
 
 class Comment extends React.Component {
   render() {
+    const comment = this.props.comment;
+    const now = moment();
+    const time = moment(comment.created_at);
+    const diff = Math.floor((now - time)/60000);
+
     return (
       <li>
         <div className="member-container">
-          <div className="card-member">TP</div>
+          <div className="card-member">tT</div>
         </div>
-        <h3>Taylor Peat</h3>
-        <div className="comment static-comment"><span>The activities are not functional.</span>
+        <h3>teamToo()</h3>
+        <div className="comment static-comment"><span>{comment.text}</span>
         </div>
-        <small>22 minutes ago - <span className="link">Edit</span> - <span className="link">Delete</span></small>
+        <small>{diff} minutes ago - <span className="link">Edit</span> - <span className="link">Delete</span></small>
         <div className="comment">
           <label>
-            <textarea required="" rows="1">The activities have not been implemented yet.</textarea>
+            <textarea
+              required=""
+              rows="1"
+              value={comment.text}
+            >
+            </textarea>
             <div>
               <a className="light-button card-icon sm-icon"></a>
               <a className="light-button smiley-icon sm-icon"></a>
