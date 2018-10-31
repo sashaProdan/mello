fc4f8b7ac2daffd34f112e99c62b4c161c0f15ef
Nick Calibey <nick.calibey@gmail.com>
2018-10-31 13:58:24 -0500

    added static CardSidebarContainer component
    Co-authored-by: nick calibey <nick.calibey@gmail.com>
    Co-authored-by: sasha prodan <prodan.sasha@gmail.com>

--- app/javascript/components/dashboard/card/CardContainer.jsx
+++ app/javascript/components/dashboard/card/CardContainer.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import * as actions from '../../../actions/CardActions';
 
 import CardHeader from './CardHeader';
 import CardMainContainer from './CardMainContainer';
+import CardSidebarContainer from './CardSidebarContainer';
 
 class CardContainer extends React.Component {
   static contextTypes = {
@@ -61,28 +61,9 @@ class CardContainer extends React.Component {
             <CardMainContainer
               card={card}
             />
-            <aside className="modal-buttons">
-              <h2>Add</h2>
-              <ul>
-                <li className="member-button"><i className="person-icon sm-icon"></i>Members</li>
-                <li className="label-button"><i className="label-icon sm-icon"></i>Labels</li>
-                <li className="checklist-button"><i className="checklist-icon sm-icon"></i>Checklist</li>
-                <li className="date-button not-implemented"><i className="clock-icon sm-icon"></i>Due Date</li>
-                <li className="attachment-button not-implemented"><i className="attachment-icon sm-icon"></i>Attachment</li>
-              </ul>
-              <h2>Actions</h2>
-              <ul>
-                <li className="move-button"><i className="forward-icon sm-icon"></i>Move</li>
-                <li className="copy-button"><i className="card-icon sm-icon"></i>Copy</li>
-                <li className="subscribe-button"><i className="sub-icon sm-icon"></i>Subscribe<i className="check-icon sm-icon"></i>
-                </li>
-                <hr />
-                <li className="archive-button"><i className="file-icon sm-icon "></i>Archive</li>
-              </ul>
-              <ul className="light-list">
-                <li className="not-implemented">Share and more...</li>
-              </ul>
-            </aside>
+            <CardSidebarContainer
+              card={card}
+            />
           </div>
         </div>
 
