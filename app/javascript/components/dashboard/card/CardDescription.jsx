eb2510ed4d39741abe8ae0bc8930b00af4b1d1eb
Nick Calibey <nick.calibey@gmail.com>
2018-10-31 12:52:58 -0500

    user can change card description via GUI
    Co-authored-by: nick calibey <nick.calibey@gmail.com>
    Co-authored-by: sasha prodan <prodan.sasha@gmail.com>

--- app/javascript/components/dashboard/card/CardDescription.jsx
+++ app/javascript/components/dashboard/card/CardDescription.jsx
@@ -2,18 +2,98 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class CardDescription extends React.Component {
+  state = {
+    editing: false,
+    description: this.props.card.description,
+    fields: {
+      description: this.props.card.description,
+    }
+  }
+
+  handleEditClick = (e) => {
+    this.setState({ editing: true });
+  }
+
+  handleCancelClick = (e) => {
+    const fields = Object.assign({}, this.state.fields, {
+      description: this.state.description,
+    });
+
+    this.setState({
+      editing: false,
+      fields,
+    });
+  }
+
+  handleChange = (e) => {
+    const fields = Object.assign({}, this.state.fields, {
+      [e.target.name]: e.target.value,
+    });
+
+    this.setState({ fields });
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    const description = this.state.fields.description;
+
+    this.props.onSubmit(e, {description})
+    this.setState({ editing: false });
+  }
+
   render() {
     const card = this.props.card;
+    const fieldDesc = this.state.fields.description
 
-    return (
-      <form className="description">
-        <p>Description</p>
-        <span id="description-edit" className="link">Edit</span>
-        <p className="textarea-overlay">{card.description}</p>
-        <p id="description-edit-options" className="hidden">You have unsaved edits on this field. <span className="link">View edits</span> - <span className="link">Discard</span>
-        </p>
-      </form>
-    )
+    if (this.state.editing) {
+      return (
+        <form
+          className="description"
+        >
+          <p>Description</p>
+          <textarea
+            name="description"
+            className="textarea-toggle"
+            rows="1"
+            value={fieldDesc}
+            onChange={this.handleChange}
+            autofocus
+          >
+          </textarea>
+          <div>
+            <div
+              className="button"
+              value="Save"
+              onClick={this.handleSubmit}
+              >
+                Save
+              </div>
+            <i
+              className="x-icon icon"
+              onClick={this.handleCancelClick}
+            >
+            </i>
+          </div>
+        </form>
+      )
+    } else {
+      return (
+        <form className="description">
+          <p>Description</p>
+          <span
+            id="description-edit"
+            className="link"
+            onClick={this.handleEditClick}
+          >
+            Edit
+          </span>
+          <p className="textarea-overlay">{card.description}</p>
+          <p id="description-edit-options" className="hidden">You have unsaved edits on this field. <span className="link">View edits</span> - <span className="link">Discard</span>
+          </p>
+        </form>
+      )
+    }
   }
 }
 
--- app/javascript/components/dashboard/card/CardDetailsContainer.jsx
+++ app/javascript/components/dashboard/card/CardDetailsContainer.jsx
@@ -1,26 +1,45 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import * as actions from '../../../actions/CardActions';
 
 import CardLabels from './CardLabels';
 import CardDueDate from './CardDueDate';
 import CardDescription from './CardDescription';
 
 class CardDetailsContainer extends React.Component {
+  static contextTypes = {
+    store: PropTypes.object,
+  };
+
+  handleSubmit = (e, attr) => {
+    e.preventDefault();
+
+    const cardId = this.props.card.id;
+    const listId = this.props.card.list_id;
+    const store = this.context.store;
+
+    store.dispatch(actions.editCard(cardId, listId, attr));
+  }
+
   render() {
     const card = this.props.card;
+    const list = this.props.list;
 
     return (
       <li className="details-section">
         <ul className="modal-details-list">
           <CardLabels
             card={card}
+            list={list}
           />
           <CardDueDate
             card={card}
+            list={list}
           />
         </ul>
           <CardDescription
             card={card}
+            onSubmit={this.handleSubmit}
           />
       </li>
     )
