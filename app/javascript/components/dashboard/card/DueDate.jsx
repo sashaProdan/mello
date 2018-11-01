f3d4ab0b247a67d95146bc9eefbe3f0cdcd24f1c
Sasha Prodan <prodan.sasha@gmail.com>
2018-11-01 12:21:12 -0500

    add DueDate and PopOver components

---
+++ app/javascript/components/dashboard/card/DueDate.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class DueDate extends React.Component {
+  static contextTypes = {
+    store: PropTypes.object,
+  };
+
+  render() {
+
+  }
+}
+
+export default DueDate;
---
+++ app/javascript/components/dashboard/card/PopOver.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class PopOver extends React.Component {
+
+  render() {
+    return {
+      <div class={`popover ${this.props.type}`}>
+
+      </div>
+    }
+  }
+}
+
+export default PopOver;
